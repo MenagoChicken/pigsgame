4ff5c6e90669289f39d53f7196b0e727e766171d
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2023-06-19 15:19:51 +0200

    end game

--- script.js
+++ script.js
@@ -16,29 +16,52 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, playing;
 
 /******************* Functions */
+
+newGame();
+
+function newGame() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+}
+
+function switchPlayer() {
+  // Set set current scores to 0
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  //Switch player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
 //Genarate random number 1-6
 function ganarateRandomNumber() {
   const randomNumber = Math.random() * 6 + 1;
   return Math.floor(randomNumber);
 }
 
-//Display the dice
 function rollTheDice() {
   if (playing) {
     const dice = ganarateRandomNumber();
-    if (diceEl.classList.contains('hidden')) {
-      diceEl.classList.remove('hidden');
-    }
+    //display the dice
+    diceEl.classList.remove('hidden');
     diceEl.setAttribute('src', `./img/dice-${dice}.png`);
 
     if (dice !== 1) {
@@ -72,16 +95,7 @@ function addToScore() {
   }
 }
 
-function switchPlayer() {
-  // Set set current scores to 0
-  currentScore = 0;
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  //Switch player
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-}
-
 /******************* Event listeners */
 btnRoll.addEventListener('click', rollTheDice);
 btnHold.addEventListener('click', addToScore);
+btnNew.addEventListener('click', newGame);
