a12f326b5bcc35b2f5b126d47e6f044b963c184f
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2023-06-19 12:52:58 +0200

    starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+// Selecting elemnets
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
